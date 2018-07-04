19fe27abd2bcb43b45592bc1559a6d82895a2a11
Ben Lipton <ben@grimm-co.com>
2018-07-04 17:29:29 -0400

    Seem to be constructing jobs properly

--- src/main.rs
+++ src/main.rs
@@ -3,19 +3,21 @@ extern crate futures;
 extern crate serde_json;
 extern crate reqwest;
 
-use futures::future;
-use hyper::{Body, Chunk, Method, Request, Response, Server, StatusCode};
-use hyper::rt::{Future, Stream};
-use hyper::service::service_fn;
+//use futures::future;
+// use hyper::{Body, Chunk, Method, Request, Response, Server, StatusCode};
+// use hyper::rt::{Future, Stream};
+// use hyper::service::service_fn;
+use reqwest::StatusCode;
 use serde_json::Value;
+use std::error::Error as StdError; // needed for `description`
 
 // Just a simple type alias
-type BoxFut = Box<Future<Item=Response<Body>, Error=hyper::Error> + Send>;
+//type BoxFut = Box<Future<Item=Response<Body>, Error=hyper::Error> + Send>;
 
+#[derive(Debug)]
 struct CIJob<'a> {
     heads: Vec<BranchHead<'a>>,
     config: &'a Config,
-    client: reqwest::Client,
 }
 
 #[derive(Debug)]
@@ -44,18 +46,22 @@ struct Error {
 }
 
 impl<'a> CIJob<'a> {
-    fn new(branch: &str, base: &str, conf: &'a Config) -> CIJob<'a> {
+    fn new(branch: &str, base: &str, conf: &'a Config) -> Result<CIJob<'a>, Error> {
         let mut job = CIJob {
             heads: Vec::with_capacity(1+conf.extra_repo_urls.len()),
             config: conf,
-            client: reqwest::Client::new(),
         };
 
         for url in &conf.extra_repo_urls {
-            job.heads.push(BranchHead::new_with_base(branch, base, url, conf));
+            let mut head = BranchHead::new(branch, url, conf)?;
+            if head.is_none() {
+                head = BranchHead::new(base, url, conf)?;
+            }
+            let head = head.ok_or(Error::from("Base branch does not exist"))?;
+            job.heads.push(head);
         }
 
-        job
+        Ok(job)
     }
 
     fn affects_status(&self, head: &BranchHead) -> bool {
@@ -112,7 +118,7 @@ impl<'a> CIJob<'a> {
 
 impl From<reqwest::Error> for Error {
     fn from(err: reqwest::Error) -> Error {
-        Error::from("reqwest")
+        Error::from(err.description())
     }
 }
 
@@ -133,26 +139,30 @@ impl<'a> BranchHead<'a> {
     }
 
     fn new(branch: &str, url: &'a str, config: &'a Config)
-            -> Result<BranchHead<'a>, Error> {
+            -> Result<Option<BranchHead<'a>>, Error> {
         let branch_url = format!("{base}/repository/commits/{branch}", base=url, branch=branch);
-        let resp: Value = reqwest::Client::new()
+        let mut resp = reqwest::Client::new()
             .get(&branch_url)
             .query(&[("private_token", &config.auth_token)])
-            .send()?
-            .json()?;
+            .send()?;
 
+        if resp.status() == StatusCode::NotFound {
+            return Ok(None);
+        }
+
+        let resp: Value = resp.json()?;
         match resp["id"].as_str() {
-            Some(commit) => Ok(BranchHead {
+            Some(commit) => Ok(Some(BranchHead {
                 branch: branch.to_string(),
                 commit: commit.to_string(),
                 repo_url: url,
                 config,
-            }),
+            })),
             None => Err(Error::from("Branch not found")),
         }
     }
 
-    fn get_status_url(&self, name: &str) -> Result<Option<String>, reqwest::Error> {
+    fn get_status_url(&self, name: &str) -> Result<Option<(String, String)>, Error> {
         let url = format!("{base}/repository/commits/{commit}/statuses",
                           base=self.repo_url, commit=self.commit);
         let resp: Value = reqwest::Client::new()
@@ -162,137 +172,147 @@ impl<'a> BranchHead<'a> {
             .send()?
             .json()?;
 
-        Ok(resp[0]["target_url"].as_str().map(str::to_string))
-    }
-}
-
-impl WebHookListener {
-    fn handle_mr_hook(&self) {
-        let branch = "";
-        let base = "";
-        let job = CIJob::new(branch, base, &self.config);
-        job.ensure_running();
-    }
-
-    fn handle_pipeline_hook(&self) {
-        let branch = "";
-        let base = "";
-        // TODO: figure out whether for a base branch or a MR
-        let job = CIJob::new(branch, base, &self.config);
-        job.update_statuses();
-    }
-
-    fn handle_push_hook(&self) {
-        let branch = "";
-        let base = "";
-        // if branch in base_branches
-        // TODO: tell job it's for the base branch, not a MR
-        let job = CIJob::new(branch, base, &self.config);
-        job.ensure_running();
-        // end if
-    }
-}
-
-fn call_ci(ref_id: &str) {
-    // trigger = "/projects/:id/trigger/pipeline"
-}
-
-fn execute_webhook(json: &Chunk) {
-    if let Ok(data) = serde_json::from_slice::<Value>(json) {
-        let kind = &data["object_kind"];
-        match kind.as_str() {
-            Some("merge_request") => {
-                println!("Was a merge request");
-                if let Value::String(ref_id) = &data["object_attributes"]["last_commit"]["id"] {
-                    call_ci(ref_id);
-                }
-            }
-            _ => {}
+        if !resp[0].is_object() {
+            return Ok(None);
+        }
+        match (resp[0]["target_url"].as_str(), resp[0]["status"].as_str()) {
+            (Some(target_url), Some(status)) =>
+                Ok(Some((status.to_string(), target_url.to_string()))),
+            _ => Err(Error::from("Response missing target_url or status field")),
         }
     }
 }
 
-fn webhook(req: Request<Body>) -> BoxFut {
-    let mut response = Response::new(Body::empty());
+impl WebHookListener {
+    fn handle_mr_hook(&self) -> Result<(), Error> {
+        let branch = "";
+        let base = "";
+        let job = CIJob::new(branch, base, &self.config)?;
+        job.ensure_running();
+        Ok(())
+    }
 
-    match req.method() {
-        &Method::POST => {
-           let future = req
-               .into_body()
-               .concat2()
-               .map(move |json| {
-                    execute_webhook(&json);
-                    response
-               });
-           return Box::new(future);
-        },
-        _ => {
-            *response.status_mut() = StatusCode::METHOD_NOT_ALLOWED;
-        },
-    };
+    fn handle_pipeline_hook(&self) -> Result<(), Error> {
+        let branch = "";
+        let base = "";
+        // TODO: figure out whether for a base branch or a MR
+        let job = CIJob::new(branch, base, &self.config)?;
+        job.update_statuses();
+        Ok(())
+    }
 
-    /*
-    match (req.method(), req.uri().path()) {
-        (&Method::GET, "/") => {
-            *response.body_mut() = Body::from("Try POSTing data to /echo");
-        },
-        (&Method::POST, "/echo") => {
-            *response.body_mut() = req.into_body();
-        },
-        (&Method::POST, "/echo/uppercase") => {
-            // This is actually a new `futures::Stream`...
-            let mapping = req
-                .into_body()
-                .map(|chunk| {
-                    chunk.iter()
-                        .map(|byte| byte.to_ascii_uppercase())
-                        .collect::<Vec<u8>>()
-                });
-
-            // Use `Body::wrap_stream` to convert it to a `Body`...
-            *response.body_mut() = Body::wrap_stream(mapping);
-        },
-        (&Method::POST, "/echo/reversed") => {
-            let reversed = req
-                .into_body()
-                .concat2()
-                .map(|chunk| {
-                    let body = chunk.iter()
-                        .rev()
-                        .cloned()
-                        .collect::<Vec<u8>>();
-                    *response.body_mut() = Body::from(body);
-                    response
-                });
-            return Box::new(reversed);
-        },
-        _ => {
-            *response.status_mut() = StatusCode::NOT_FOUND;
-        },
-    };
-    */
-
-    Box::new(future::ok(response))
+    fn handle_push_hook(&self) -> Result<(), Error> {
+        let branch = "";
+        let base = "";
+        // if branch in base_branches
+        // TODO: tell job it's for the base branch, not a MR
+        let job = CIJob::new(branch, base, &self.config)?;
+        job.ensure_running();
+        Ok(())
+        // end if
+    }
 }
 
-fn run_server() {
-    // This is our socket address...
-    let addr = ([127, 0, 0, 1], 3000).into();
-
-    // A `Service` is needed for every connection, so this
-    // creates on of our `hello_world` function.
-    let new_svc = || {
-        // service_fn_ok converts our function into a `Service`
-        service_fn(webhook)
-    };
-
-    let server = Server::bind(&addr)
-        .serve(new_svc)
-        .map_err(|e| eprintln!("server error: {}", e));
-
-    // Run this server for... forever!
-    hyper::rt::run(server);
-}
+// fn call_ci(ref_id: &str) {
+//     // trigger = "/projects/:id/trigger/pipeline"
+// }
+// 
+// fn execute_webhook(json: &Chunk) {
+//     if let Ok(data) = serde_json::from_slice::<Value>(json) {
+//         let kind = &data["object_kind"];
+//         match kind.as_str() {
+//             Some("merge_request") => {
+//                 println!("Was a merge request");
+//                 if let Value::String(ref_id) = &data["object_attributes"]["last_commit"]["id"] {
+//                     call_ci(ref_id);
+//                 }
+//             }
+//             _ => {}
+//         }
+//     }
+// }
+// 
+// fn webhook(req: Request<Body>) -> BoxFut {
+//     let mut response = Response::new(Body::empty());
+// 
+//     match req.method() {
+//         &Method::POST => {
+//            let future = req
+//                .into_body()
+//                .concat2()
+//                .map(move |json| {
+//                     execute_webhook(&json);
+//                     response
+//                });
+//            return Box::new(future);
+//         },
+//         _ => {
+//             *response.status_mut() = StatusCode::METHOD_NOT_ALLOWED;
+//         },
+//     };
+// 
+//     /*
+//     match (req.method(), req.uri().path()) {
+//         (&Method::GET, "/") => {
+//             *response.body_mut() = Body::from("Try POSTing data to /echo");
+//         },
+//         (&Method::POST, "/echo") => {
+//             *response.body_mut() = req.into_body();
+//         },
+//         (&Method::POST, "/echo/uppercase") => {
+//             // This is actually a new `futures::Stream`...
+//             let mapping = req
+//                 .into_body()
+//                 .map(|chunk| {
+//                     chunk.iter()
+//                         .map(|byte| byte.to_ascii_uppercase())
+//                         .collect::<Vec<u8>>()
+//                 });
+// 
+//             // Use `Body::wrap_stream` to convert it to a `Body`...
+//             *response.body_mut() = Body::wrap_stream(mapping);
+//         },
+//         (&Method::POST, "/echo/reversed") => {
+//             let reversed = req
+//                 .into_body()
+//                 .concat2()
+//                 .map(|chunk| {
+//                     let body = chunk.iter()
+//                         .rev()
+//                         .cloned()
+//                         .collect::<Vec<u8>>();
+//                     *response.body_mut() = Body::from(body);
+//                     response
+//                 });
+//             return Box::new(reversed);
+//         },
+//         _ => {
+//             *response.status_mut() = StatusCode::NOT_FOUND;
+//         },
+//     };
+//     */
+// 
+//     Box::new(future::ok(response))
+// }
+// 
+// fn run_server() {
+//     // This is our socket address...
+//     let addr = ([127, 0, 0, 1], 3000).into();
+// 
+//     // A `Service` is needed for every connection, so this
+//     // creates on of our `hello_world` function.
+//     let new_svc = || {
+//         // service_fn_ok converts our function into a `Service`
+//         service_fn(webhook)
+//     };
+// 
+//     let server = Server::bind(&addr)
+//         .serve(new_svc)
+//         .map_err(|e| eprintln!("server error: {}", e));
+// 
+//     // Run this server for... forever!
+//     hyper::rt::run(server);
+// }
 
 fn main() {
     let config = Config {
@@ -302,11 +322,6 @@ fn main() {
         auth_token: "xQjkvDxxpu-o2ny4YNUo".to_string(),
     };
 
-    let bh = BranchHead::new("master", &config.pipeline_url, &config).unwrap();
-
-    if let Some(status_url) = bh.get_status_url("default").unwrap() {
-        println!("status_url: {}", status_url);
-    } else {
-        println!("No status url");
-    }
+    let job = CIJob::new("foo", "master", &config).unwrap();
+    println!("job: {:?}", job);
 }
